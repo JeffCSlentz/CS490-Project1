e66b3f5a3a50605a7a834f9eb2ebea311a79783b
JeffCSlentz <Jcszk9@umkc.edu>
2015-10-20 15:56:31 -0500

    Update gridMovieGen.js

--- Site-Root/script/gridMovieGen.js
+++ Site-Root/script/gridMovieGen.js
@@ -6,7 +6,7 @@ function Gallery(data){
     this.movies = data;
     this.list_icon = "#list_icon";
     this.grid_icon = "#grid_icon";
-    this.grid_movie_div = "#gridWrapper";
+    this.movies_div = "#movies";
     this.grid_movie_template = "templates/grid_photo.html";
     
     var self = this;
@@ -28,20 +28,21 @@ Gallery.prototype.load_grid_movies = function(self){
     $.get(self.grid_movie_template, function(template){
         var html_maker = new htmlMaker(template);
         var html = html_maker.getHTML(self.movies);
-        $(self.grid_movie_div).html(html);
+        $(self.movies_div).html(html);
     });
 };
 
 Gallery.prototype.make_grid = function () {
-    $(this.grid_movie_div).attr("class", "gridWrapper");
+    $(this.movies_div).attr("class", "grid");
     $(this.grid_icon).attr("src", "Data/Icons/grid_pressed.jpg");
     $(this.list_icon).attr("src", "Data/Icons/list.jpg");
 };
 
 Gallery.prototype.make_list = function () {
-    $(this.grid_movie_div).attr("class", "listWrapper");
+    $(this.movies_div).attr("class", "list");
     $(this.grid_icon).attr("src", "Data/Icons/grid.jpg");
     $(this.list_icon).attr("src", "Data/Icons/list_pressed.jpg");
 };
+
 
 
